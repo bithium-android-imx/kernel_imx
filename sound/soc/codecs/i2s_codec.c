9c1d0b4fdddc5e26841f77c69b04d0371ca39827
Filipe Alves <developer@bithium.com>
2020-01-28 18:49:48 +0000

    REM: Code related to unsupported features.

--- sound/soc/codecs/i2s_codec.c
+++ sound/soc/codecs/i2s_codec.c
@@ -32,88 +32,31 @@
 
 static const struct snd_soc_dapm_widget i2s_codec_dapm_widgets[] = {
    SND_SOC_DAPM_INPUT("LINE_IN"),
-   SND_SOC_DAPM_INPUT("MIC_IN"),
-
-   SND_SOC_DAPM_OUTPUT("HP_OUT"),
    SND_SOC_DAPM_OUTPUT("LINE_OUT"),
 };
 
 /* routes for i2s_codec */
 static const struct snd_soc_dapm_route i2s_codec_dapm_routes[] = {
    {"Capture", NULL, "LINE_IN"}, /* line_in --> adc_mux */
-   {"Capture", NULL, "MIC_IN"},   /* mic_in --> adc_mux */
-
    {"LINE_OUT", NULL, "Playback"},
-   {"HP_OUT", NULL, "Playback"},
 };
-
-/* custom function to fetch info of PCM playback volume */
-static int dac_info_volsw(struct snd_kcontrol *kcontrol,
-           struct snd_ctl_elem_info *uinfo)
-{
-   uinfo->type = SNDRV_CTL_ELEM_TYPE_INTEGER;
-   uinfo->count = 2;
-   uinfo->value.integer.min = 0;
-   uinfo->value.integer.max = 0xfc - 0x3c;
-   return 0;
-}
-
-static int dac_get_volsw(struct snd_kcontrol *kcontrol,
-          struct snd_ctl_elem_value *ucontrol)
-{
-   ucontrol->value.integer.value[0] = 0x3c;
-   ucontrol->value.integer.value[1] = 0x3c;
-
-   return 0;
-}
-
-static int dac_put_volsw(struct snd_kcontrol *kcontrol,
-          struct snd_ctl_elem_value *ucontrol)
-{
-   return 0;
-}
 
 static const struct snd_kcontrol_new i2s_codec_snd_controls[] = {
    /* SOC_DOUBLE_S8_TLV with invert */
    {
       .iface = SNDRV_CTL_ELEM_IFACE_PCM,
       .name = "PCM Playback Volume",
-      .access = SNDRV_CTL_ELEM_ACCESS_TLV_READ |
-         SNDRV_CTL_ELEM_ACCESS_READWRITE,
-      .info = dac_info_volsw,
-      .get = dac_get_volsw,
-      .put = dac_put_volsw,
+      .access = SNDRV_CTL_ELEM_ACCESS_INACTIVE,
+      .info = NULL,
+      .get = NULL,
+      .put = NULL,
    }
 };
 
-/* mute the codec used by alsa core */
-static int i2s_codec_digital_mute(struct snd_soc_dai *codec_dai, int mute)
-{
-   return 0;
-}
-
-/* set codec format */
-static int i2s_codec_set_dai_fmt(struct snd_soc_dai *codec_dai, unsigned int fmt)
-{
-   return 0;
-}
-
-static int i2s_codec_pcm_hw_params(struct snd_pcm_substream *substream,
-              struct snd_pcm_hw_params *params,
-              struct snd_soc_dai *dai)
-{
-   return 0;
-}
-
-#define I2S_CODEC_FORMATS (SNDRV_PCM_FMTBIT_S16_LE |\
-         SNDRV_PCM_FMTBIT_S20_3LE |\
-         SNDRV_PCM_FMTBIT_S24_LE |\
-         SNDRV_PCM_FMTBIT_S32_LE)
-
 static const struct snd_soc_dai_ops i2s_codec_ops = {
-   .hw_params = i2s_codec_pcm_hw_params,
-   .digital_mute = i2s_codec_digital_mute,
-   .set_fmt = i2s_codec_set_dai_fmt,
+   .hw_params = NULL,
+   .digital_mute = NULL,
+   .set_fmt = NULL,
 };
 
 static struct snd_soc_dai_driver i2s_codec_dai = {
@@ -138,7 +81,7 @@ static struct snd_soc_dai_driver i2s_codec_dai = {
 
 static struct snd_soc_codec_driver i2s_codec_driver = {
    .component_driver = {
-      .controls      = i2s_codec_snd_controls,
+      .controls         = i2s_codec_snd_controls,
       .num_controls     = ARRAY_SIZE(i2s_codec_snd_controls),
       .dapm_widgets     = i2s_codec_dapm_widgets,
       .num_dapm_widgets = ARRAY_SIZE(i2s_codec_dapm_widgets),
@@ -149,7 +92,7 @@ static struct snd_soc_codec_driver i2s_codec_driver = {
 
 static int i2s_codec_codec_probe(struct platform_device *pdev)
 {
-	struct device *dev = &pdev->dev;
+   struct device *dev = &pdev->dev;
    int ret;
 
    dev_info(dev, "i2s_codec probe start\n");
