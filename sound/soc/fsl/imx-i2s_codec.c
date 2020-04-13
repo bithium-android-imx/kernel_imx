a6774db13f4a890b1946bb6a0be2896c9c9639da
Bithium S.A <development@bithium.com>
2020-04-13 13:49:58 +0100

    CHG: More code cleanup in imx-i2s-codec

--- sound/soc/fsl/imx-i2s_codec.c
+++ sound/soc/fsl/imx-i2s_codec.c
@@ -29,11 +29,8 @@ struct imx_i2s_codec_data {
 };
 
 static const struct snd_soc_dapm_widget imx_i2s_codec_dapm_widgets[] = {
-	SND_SOC_DAPM_MIC("Mic Jack", NULL),
 	SND_SOC_DAPM_LINE("Line In Jack", NULL),
-	SND_SOC_DAPM_HP("Headphone Jack", NULL),
 	SND_SOC_DAPM_SPK("Line Out Jack", NULL),
-	SND_SOC_DAPM_SPK("Ext Spk", NULL),
 };
 
 static int imx_i2s_codec_probe(struct platform_device *pdev)
@@ -46,43 +43,6 @@ static int imx_i2s_codec_probe(struct platform_device *pdev)
 	int ret;
 
 	dev_info(&pdev->dev, "imx-i2s_codec probe start\n");
-	if (!of_property_read_bool(np, "fsl,no-audmux")) {
-		ret = of_property_read_u32(np, "mux-int-port", &int_port);
-		if (ret) {
-			dev_err(&pdev->dev, "mux-int-port missing or invalid\n");
-			return ret;
-		}
-		ret = of_property_read_u32(np, "mux-ext-port", &ext_port);
-		if (ret) {
-			dev_err(&pdev->dev, "mux-ext-port missing or invalid\n");
-			return ret;
-		}
-
-		/*
-		 * The port numbering in the hardware manual starts at 1, while
-		 * the audmux API expects it starts at 0.
-		 */
-		int_port--;
-		ext_port--;
-		ret = imx_audmux_v2_configure_port(int_port,
-				IMX_AUDMUX_V2_PTCR_SYN |
-				IMX_AUDMUX_V2_PTCR_TFSEL(ext_port) |
-				IMX_AUDMUX_V2_PTCR_TCSEL(ext_port) |
-				IMX_AUDMUX_V2_PTCR_TFSDIR |
-				IMX_AUDMUX_V2_PTCR_TCLKDIR,
-				IMX_AUDMUX_V2_PDCR_RXDSEL(ext_port));
-		if (ret) {
-			dev_err(&pdev->dev, "audmux internal port setup failed\n");
-			return ret;
-		}
-		ret = imx_audmux_v2_configure_port(ext_port,
-				IMX_AUDMUX_V2_PTCR_SYN,
-				IMX_AUDMUX_V2_PDCR_RXDSEL(int_port));
-		if (ret) {
-			dev_err(&pdev->dev, "audmux external port setup failed\n");
-			return ret;
-		}
-	}
 
 	ssi_np = of_parse_phandle(pdev->dev.of_node, "ssi-controller", 0);
 	codec_np = of_parse_phandle(pdev->dev.of_node, "audio-codec", 0);
@@ -118,9 +78,7 @@ static int imx_i2s_codec_probe(struct platform_device *pdev)
 	ret = snd_soc_of_parse_card_name(&data->card, "model");
 	if (ret)
 		goto fail;
-	ret = snd_soc_of_parse_audio_routing(&data->card, "audio-routing");
-	if (ret)
-		goto fail;
+
 	data->card.num_links = 1;
 	data->card.owner = THIS_MODULE;
 	data->card.dai_link = &data->dai;
